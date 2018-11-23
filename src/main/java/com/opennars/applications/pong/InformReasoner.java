6c4b6b606c4c7c34accf5970326e5adda24d2305
PtrMan <robertw89@googlemail.com>
2018-11-23 14:39:14 +0100

    Refactor: InformReasoner

--- src/main/java/com/opennars/applications/pong/InformReasoner.java
+++ src/main/java/com/opennars/applications/pong/InformReasoner.java
@@ -1,7 +1,7 @@
 package com.opennars.applications.pong;
 
-import com.opennars.applications.crossing.*;
-import org.opennars.main.Nar;
+import com.opennars.applications.componentbased.Entity;
+import org.opennars.interfaces.NarseseConsumer;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,37 +12,29 @@ public class InformReasoner {
     List<String> inputs = new ArrayList<>();
 
     //minX and minY define the lower end of the relative coordinate system
-    public void informAboutEntity(Nar nar, Entity ent, int minX, int minY) {
-        String id = String.valueOf(ent.id);
+    public void informAboutEntity(Entity entity) {
+        String id = String.valueOf(entity.id);
         boolean useMultipleIDs = true;
         if(!useMultipleIDs) {
             id = "0";
         }
-        String pos = Util.positionToTerm((int) ent.posX-minX, (int) ent.posY-minY);
-        if (ent instanceof Car) {
+        String pos = Util.positionToTerm((int) entity.posX-minX, (int) entity.posY-minY);
+        if (entity instanceof Car) {
             inputs.add("<(*,car" + id + ","+ pos + ") --> at>. :|:");
             input += inputs.get(inputs.size()-1);
         }
-        if (ent instanceof Pedestrian) {
+        if (entity instanceof Pedestrian) {
             inputs.add("<(*,pedestrian" + id + "," + pos + ") --> at>. :|:");
             input += inputs.get(inputs.size()-1);
         }
     }
 
-    public void informAboutTrafficLight(Nar nar, TrafficLight light, int minX, int minY) {
-        //String id = String.valueOf(light.id);
-        String colour = light.colour == 0 ? "green" : "red";
-        String narsese = "<trafficLight --> ["+colour+"]>. :|:";
-        inputs.add(narsese);
-        input+=narsese;
-    }
-
     // /param force are the inputs forced to be fed into the reasoner
-    public boolean Input(Nar nar, final boolean force) {
+    public boolean Input(NarseseConsumer consumer, final boolean force) {
         boolean hadInput = false;
         if(!input.equals(lastInput)||force) {
             for(String inp : inputs) {
-                nar.addInput(inp);
+                consumer.addInput(inp);
                 hadInput = true;
             }
             lastInput = input;
