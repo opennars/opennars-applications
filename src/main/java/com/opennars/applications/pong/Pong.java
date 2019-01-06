0552157de912e60895a021aff88d7e17f6351118
PtrMan <robertw89@googlemail.com>
2019-01-06 06:08:06 +0100

    Update: tuning random chance - seems to play better with some randomness

--- src/main/java/com/opennars/applications/pong/Pong.java
+++ src/main/java/com/opennars/applications/pong/Pong.java
@@ -237,7 +237,7 @@ public class Pong extends PApplet {
             { // inject random op from time to time by chance to avoid getting stuck in cycles from which the agent can't escape
                 int rngValue2 = rng.nextInt( 100);
 
-                int chance = 0; // in percentage
+                int chance = 15; // in percentage
 
                 if (rngValue2 < chance) {
                     System.out.println("[d] FORCED random op");
