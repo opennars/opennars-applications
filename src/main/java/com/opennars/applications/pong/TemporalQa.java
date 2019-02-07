fe3c5d6109e604508c829b4aea7a57b47246b830
PtrMan <robertw89@googlemail.com>
2019-02-07 11:41:27 +0100

    Update: "pong": updated defaultConfig with used settings, improved "temporal Q&A" - emits the ^op form

--- src/main/java/com/opennars/applications/pong/TemporalQa.java
+++ src/main/java/com/opennars/applications/pong/TemporalQa.java
@@ -19,7 +19,7 @@ public class TemporalQa {
     public List<Object> sequence = new ArrayList<>();
 
 
-    public int bufferLength = 20;
+    public int bufferLength = 500;
     public int horizon = 5;
 
     public TemporalQa(NarseseConsumer consumer) {
@@ -31,6 +31,7 @@ public class TemporalQa {
 
         if (term.equals(goalTerms.get(0))) {
             informReasoner();
+            informReasonerAboutPrecondOpGoalForm();
         }
     }
 
@@ -39,6 +40,7 @@ public class TemporalQa {
 
         if (narsese.equals(goalTerms.get(0).toString())) {
             informReasoner();
+            informReasonerAboutPrecondOpGoalForm();
         }
     }
 
@@ -59,17 +61,64 @@ public class TemporalQa {
     }
 
     void informReasoner() {
-        for(int startIdx=sequence.size()-1-horizon;startIdx<sequence.size()-1-1;startIdx++) {
-            informReasonerSlice(startIdx, sequence.size()-1);
+        informReasonerOfPossibleSlices(sequence);
+    }
+
+    // informs the reasoner about (&/, C, OP) =/> G if it appears
+    void informReasonerAboutPrecondOpGoalForm() {
+        List<Object> conditionalSlice = new ArrayList<>();
+
+        // we need to kepp track of the accumulated time between the goal and the first op!
+        long accumulatedTime = 0;
+
+        // scan from back to front and skip last sequence element
+        boolean wasOpScaned = false;
+        for(int idx=sequence.size()-1-1;idx>=0;idx--) {
+            Object io = sequence.get(idx);
+
+            // accumulate time before op
+            if (!wasOpScaned && io.toString().contains("+")) {
+                String intervalTimeAsString = io.toString().substring(1, io.toString().length());
+
+                accumulatedTime += Integer.parseInt(intervalTimeAsString);
+            }
+
+            if (io.toString().contains("up") || io.toString().contains("down")) { // is it a op
+                wasOpScaned = true;
+            }
+
+            if (wasOpScaned) {
+                conditionalSlice.add(0, io);
+            }
+        }
+
+        if (wasOpScaned) {
+            List<Object> sequence2 = conditionalSlice; // TODO< copy >
+
+            if (accumulatedTime > 0) {
+                sequence2.add(new Interval(accumulatedTime));
+            }
+
+            sequence2.add(sequence.get(sequence.size()-1));
+
+            informReasonerOfPossibleSlices(conditionalSlice);
         }
     }
 
-    void informReasonerSlice(int startIdx, int endIdx) {
+    void informReasonerOfPossibleSlices(final List<Object> arr) {
+        for(int startIdx=arr.size()-1-horizon;startIdx<arr.size()-1-3;startIdx++) {
+            List<Object> slice = arr.subList(startIdx, arr.size()-1+1);
+            informReasonerSlice(slice);
+        }
+    }
+
+
+    void informReasonerSlice(List<Object> slice) {
         String narsese = "";
 
         // build components of sequence
-        for(int idx=startIdx;idx<=endIdx-1;idx++) {
-            narsese += sequence.get(idx).toString() + ",";
+        for(int idx=0;idx<=slice.size()-1-1;idx++) {
+            narsese += slice.get(idx).toString() + ",";
         }
 
         if (narsese.length() > 0) {
@@ -77,7 +126,8 @@ public class TemporalQa {
             int debug = 5;
         }
 
-        String narseseTemporalImplPred = sequence.get(endIdx).toString();
+        int endIdx = slice.size()-1;
+        String narseseTemporalImplPred = slice.get(endIdx).toString();
 
         // build conclusion
         String finalNarsese = "<" + "(&/," + narsese + ")" + " =/> " + narseseTemporalImplPred + ">.";
